cbdcbdee2348cdba6cb6cc014896684acd7c77ef
QQQYG <1371703802@qq.com>
2026-05-14 15:40:29 +0800

    添加项目文件。

---
+++ SEU_CNN_Project/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+// 测试附加包含目录是否生效
+#include "core/Tensor.h" 
+#include "layers/ConvLayer.h"
+
+int main() {
+    std::cout << "CNN MNIST Project Initialized Successfully!" << std::endl;
+    return 0;
+}
